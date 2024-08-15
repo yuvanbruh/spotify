88a5ae56a7ff0ff99b80de420ced2a56598df6b0
yuvanbruh <141669130+yuvanbruh@users.noreply.github.com>
2024-08-15 16:01:33 +0530

    Update spotify.js

--- spotify.js
+++ spotify.js
@@ -1,236 +1,103 @@
-// console.log("this iss")
-
-// let songIndex=0;
-// let song=new Audio("songs/1.mp3");
-// let masterplay = document.getElementById('masterplay')
-// let myProgressBar=document.getElementById('myProgressBar')
-// let gif=document.getElementById('gif')
-
-// let lalaland= document.getElementById('lalaland')
-
-
-
-// let songs=[
-//     {songName:"let me love you ", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"}
-// ]
-
-// masterplay.addEventListener('click',()=>{
-//     if(song.paused || song.currentTime <=0){
-//         song.play()
-//         masterplay.classList.remove('fa-circle-play');
-//         masterplay.classList.add("fa-circle-pause");
-//         lalaland.style.opacity=1
-//     }
-//     else{
-//         song.paused()
-//         masterplay.classList.remove('fa-circle-pause');
-//         masterplay.classList.add("fa-circle-play");
-//         lalaland.style.opacity=0
-         
-//     }
-
-
-
-
-// })
-
-
-// // gif.addEventListener('click', ()=>{
-// //     if(song.paused || song.currentTime<=0){
-// //         song.play()
-// //         gif.classList.remove('fa-circle-play');
-// //         gif.classList.add("fa-circle-pause");
-// //     }
-// //     else{
-// //         song.pause()
-// //         gif.classList.remove('fa-circle-pause');
-// //         gif.classList.add("fa-circle-play");
-        
-// //     }
-        
-// //     }
-// // )
-
-
-
-
-// song=document.addEventListener('timeupdate',()=>{
-//     console.log('timeupdate')
-// })
-
-// console.log("hello there ")
+let audioElement=new Audio(`songs/1.mp3`)
+let songItem=Array.from(document.getElementsByClassName("songItem"))
 let songIndex=0
-// let song = new Audio('songs/1.mp3')
-let song =new Audio('The-Neighbourhood-Sweater-Weather-(RawPraise.ng).mp3')
-//  let songs=[
-//     {
-//         songName:"let me love you", filePath:'songs/1.mp3', coverPath:"covers/1.jpg",
-//         songName:"city", filePath:'songs/2.mp3', coverPath:"covers/2.jpg",
-//         songName:"chelsea", filePath:'songs/3.mp3', coverPath:"covers/3.jpg",
-//         songName:"want it hard", filePath:'songs/4.mp3', coverPath:"covers/4.jpg",
-//         songName:"hard me ", filePath:'songs/5.mp3', coverPath:"covers/5.jpg",
-//         songName:"vampire", filePath:'songs/6.mp3', coverPath:"covers/6.jpg",
-//         songName:"summer time sadness", filePath:'songs/7.mp3', coverPath:"covers/7.jpg",
-//         songName:"luis diaz", filePath:'songs/8.mp3', coverPath:"covers/8.jpg"
-//     }
+let masterPlay=document.getElementById("masterPlay")
+let previous =document.getElementById("previous")
+let next =document.getElementById("next")
+let masterSongName=document.getElementById("masterSongName")
+let myProgressBar=document.getElementById("myProgressBar")
 
-//  ]
+let songs=[
 
- let yuvan= document.getElementById('masterplay')
- masterplay.addEventListener('click',()=>{
-    if(song.paused || song.currentTime<=0){
-        song.play()
-        yuvan.classList.remove("fa-circle-play")
-        yuvan.classList.add("fa-circle-pause")
+    {songName: "prettry girl", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "pretty boy", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "sweather weather", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "muse", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "how you doin", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
+    {songName: "oke oka", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
+    {songName: "lokham", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
+    {songName: "high", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
+    {songName: "Teri kasam", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
+    {songName: "im cahndker", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
+]
 
+songItem.forEach((e,i)=>{
+    e.getElementsByClassName("songName")[0].innerText=songs[i].songName
+    e.getElementsByTagName("img")[0].src=songs[i].coverPath
+})
+
+masterPlay.addEventListener("click",()=>{
+    if(audioElement.paused || audioElement.src<0){
+        audioElement.play()
+        masterPlay.classList.remove("fa-play-circle")
+        masterPlay.classList.add("fa-pause-circle")
     }
     else{
-        song.pause()
-        yuvan.classList.remove("fa-circle-pause")
-        yuvan.classList.add("fa-circle-play")
+        audioElement.pause()
+        masterPlay.classList.remove("fa-pause-circle")
+        masterPlay.classList.add("fa-play-circle")
+
     }
-
- })
-
-//  let virat=document.getElementById('lalaland')
-//  lalaland.addEventListener('click',()=>{
-//     if(song.paused || song1.currentTime<=0){
-//         song.play()
-//         virat.classList.remove("fa-circle-play")
-//         virat.classList.add("fa-circle-pause")
-//     }
-//     else{
-//         song1.pause()
-//         virat.classList.remove("fa-circle-pause")
-//         virat.classList.add("fa-circle-play")
-//     }
-
-//  })
-
-
-// let music= new Audio('The-Neighbourhood-Sweater-Weather-(RawPraise.ng).mp3')
-
-// let radacanu=document.getElementById('cool')
-// radacanu.addEventListener('click',()=>{
-//     music.play()
-// }) 
-// let emma= new Audio("The-Neighbourhood-Sweater-Weather-(RawPraise.ng).mp3")
-// let rohit = document.getElementById('mess')
-// rohit.addEventListener("click",()=>{
-//  if(emma.paused || emma.currentTime<=0){
-//     emma.play()
-//     rohit.classList.remove('fa-circle-play')
-//     rohit.classList.add('fa-circle-pause')
-//  }
-//  else{
-//     emma.pause()
-//     rohit.classList.remove('fa-circle-pause')
-//     rohit.classList.add('fa-circle-play')
-//  }
-  
-// })
-
-
-// let coin= document.getElementById('solidworks')
-// solidworks.addEventListener('click',()=>{
-//      song.play()
-// })
-
-song.addEventListener('timeupdate',()=>{
-    console.log('timeupdate')
-})
-song.addEventListener('timeupdate',()=>{
-    console.log('timeupdate')
-
 })
 
-
-develop= parseInt((song.currentTime/song.duration)*100)
-console.log('develop')
-
-myProgressBar.value= develop;
-myProgressBar.value=develop;
-
-myProgressBar.addEventListener('change',()=>{
-    song.currentTime=myProgressBar.value * song.duration/100
-})
-
-// let olivia=document.getElementById('track')
-// track.addEventListener('click',()=>{
-//     if(song.paused || song1.currentTime<=0){
-//         song.play()
-//         olivia.classList.remove('fa-circle-play')
-//         olivia.classList.add("fa-circle-pause")
-
-//     }
-//     else{
-//         olivia.classList.remove('fa-circle-pause')
-//         olivia.classList.add("fa-circle-play")
-
-//     }
-// })
-// let songItems= Array.from(document.getElementById('u'))
-// songItems.forEach((element,i)=>{
-    
-//     element.geElementsByTagName("img")[0].src=song[i].coverPath
-//     element.geElementByClassName("txt")[0].innerText=song[i].txt
-
-// })
-
-const makeAllPlays=()=>{
-
-    
- Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
-    element.classList.add('fa-circle-play')
-    element.classList.add('fa-circle-pause')
- })
-
-}
-
-
-
- Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
-    element.addEventListener('click',(e)=>{
-        console.log(e)
-        makeAllPlays()
-        songIndex = parseInt(e.target.id)
-        e.target.classList.remove('fa-circle-play')
-        e.target.classList.add('fa-circle-pause')
-        song.src=`The-Neighbourhood-Sweater-Weather-(RawPraise.ng).mp3`
-        song.currentTime=0
-        song.play()
-        yuvan.classList.remove("fa-circle-play")
-        yuvan.classList.add("fa-circle-pause")
-        
-
+Array.from(document.getElementsByClassName("songItemPlay")).forEach((element)=>{
+    element.addEventListener("click",(e)=>{
+    songIndex=parseInt(e.target.id)
+    console.log(songIndex)
+    if(audioElement.paused || audioElement.src<0){
+        e.target.classList.remove("fa-play-circle")
+        e.target.classList.add("fa-pause-circle")
+        masterPlay.classList.remove("fa-play-circle")
+        masterPlay.classList.add("fa-pause-circle")
+        audioElement.src=`songs/${songIndex+1}.mp3`
+        audioElement.play()
+        masterSongName.innerText=songs[songIndex].songName
+    }
+    else{
+        audioElement.pause()
+        e.target.classList.remove("fa-pause-circle")
+    e.target.classList.add("fa-play-circle")
+    masterPlay.classList.remove("fa-pause-circle")
+    masterPlay.classList.add("fa-play-circle")
+    }
     })
-
- })
-
-document.getElementById('next').addEventListener('click',()=>{
-    if(songIndex>=9){
+})
+next.addEventListener("click",(i)=>{
+    if(songIndex<0){
         songIndex=0
     }
     else{
         songIndex+=1
     }
-    song.src=`songs/${songIndex+1}.mp3`
-    song.currentTime=0
-    song.play()
-    yuvan.classList.remove("fa-circle-play")
-    yuvan.classList.add("fa-circle-pause")
+    audioElement.src=`songs/${songIndex+1}.mp3`
+    masterSongName.innerText=songs[songIndex].songName
+    audioElement.play()
+    masterPlay.classList.remove("fa-play-circle")
+    masterPlay.classList.add("fa-pause-circle")
+
 })
 
-document.getElementById('previous').addEventListener('click',()=>{
-    if(songIndex<=0){
+previous.addEventListener("click",(i)=>{
+    if(songIndex<0){
         songIndex=0
     }
     else{
         songIndex-=1
     }
-    song.src=`songs/${songIndex+1}.mp3`
-    song.currentTime=0
-    song.play()
-    yuvan.classList.remove("fa-circle-play")
-    yuvan.classList.add("fa-circle-pause")
+    audioElement.src=`songs/${songIndex+1}.mp3`
+    masterSongName.innerText=songs[songIndex].songName
+    audioElement.play()
+    masterPlay.classList.remove("fa-play-circle")
+    masterPlay.classList.add("fa-pause-circle")
+
+})
+
+audioElement.addEventListener('timeupdate', ()=>{ 
+    // Update Seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)*100); 
+    myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener("change",()=>{
+    audioElement.currentTime=myProgressBar.value*audioElement.duration/100
 })
